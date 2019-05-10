2eaff052036fb83c9d1e47cf09d958cad0d035f8
Andrew Ormond <drewthedev9@gmail.com>
2019-05-09 23:09:30 -0400

    changed

--- server.js
+++ server.js
@@ -13,8 +13,10 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-      connectionString : process.env.DATABASE_URL,
-      ssl: 'true',
+    host: '127.0.01',
+    user : 'andrew',
+    password: 'andrew',
+    database: 'smartbrain'
     }
   });
 
